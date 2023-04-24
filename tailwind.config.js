0346280279a4f944dbc4ddc59348b79b5db08e9e
Nicolas Cinzer <ncinzer@os.aeroterra.com>
2023-04-23 21:38:01 -0500

    configs and more

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateAreas: {
+        prs: ['excersice excersice', 'times lift'],
+        home: ['prs mc', 'prs stats'],
+      },
+      gridTemplateColumns: {
+        home: '1fr 4fr',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@savvywombat/tailwindcss-grid-areas')],
 };
