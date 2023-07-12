671b6e91dea2f8c00962e4410ad7036515a0dd8a
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-12 00:20:56 -0500

    Navbar with PR & Exercises

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,24 @@ type Props = {}
 export default function Navbar({}: Props) {
   return (
     <nav className='flex justify-evenly items-center text-acc-color py-2 border-b mx-4'>
-      <Link href={'/prs'}>PRs</Link>
+      <Link
+        href={'/prs'}
+        className='hover:text-comp-color transition-all duration-200'
+      >
+        PRs
+      </Link>
       <Link
         href={'/'}
         className='text-2xl hover:text-comp-color transition-all duration-200 hover:tracking-widest'
       >
         POSSE
       </Link>
-      <Link href={'/exercises'}>EXERCISES</Link>
+      <Link
+        href={'/exercises'}
+        className='hover:text-comp-color transition-all duration-200'
+      >
+        EXERCISES
+      </Link>
     </nav>
   )
 }
