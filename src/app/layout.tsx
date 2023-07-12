6c59215e03d7587155d0516e1989f324c36c84bb
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-11 22:33:22 -0500

    svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const font = Advent_Pro({ subsets: ['latin'] })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={`${font.className} flex flex-col h-screen p-4`}>
+      <body className={`${font.className} flex flex-col h-screen `}>
         <Navbar />
         {children}
       </body>
