e0560c0aa03219e9cb728b100db311b01db254c9
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-11 22:08:36 -0500

    Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      colors: {
+        'dom-color': 'var(--dom-color)',
+        'acc-color': 'var(--acc-color)',
+        'comp-color': 'var(--comp-color)'
+      },
       gridTemplateAreas: {
         prs: ['exercise exercise', 'times lift'],
-        home: ['prs mc', 'prs stats'],
+        home: ['prs mc', 'prs stats']
       },
       gridTemplateColumns: {
-        home: '1fr 4fr',
-      },
-    },
+        home: '1fr 4fr'
+      }
+    }
   },
-  plugins: [require('@savvywombat/tailwindcss-grid-areas')],
-};
+  plugins: [require('@savvywombat/tailwindcss-grid-areas')]
+}
---
+++ types.d.ts
@@ -0,0 +1,63 @@
+type BlockType = {
+  id: number
+  name: string
+}
+
+type Movement = {
+  id: number
+  name: string
+}
+
+type Program = {
+  id: number
+  name: string
+}
+
+type Block = {
+  id: number
+  typeID: number
+  movementID: number
+  duration: number
+}
+
+type Exercise = {
+  id: number
+  movementID?: number
+  name: string
+  type: 'Basic' | 'Variant' | 'Accessory' | null
+}
+
+type Mesocycle = {
+  id: number
+  blockID: number
+  duration: number
+}
+
+type PRs = {
+  id: number
+  exerciseID: number
+  series: number
+  weight: number
+  reps: number
+  unit: 'LBS' | 'KGs'
+  comments: string
+}
+
+type RutineDay = {
+  id: number
+  blockID: number
+  mesocycleID: number
+  week: number
+  day: number
+}
+
+type RutineDayExercise = {
+  id: number
+  rutineDayID: number
+  exerciseID: number
+  series: number
+  weight: number
+  reps: number
+  unit: 'LBS' | 'KGs'
+  comments: string
+}
