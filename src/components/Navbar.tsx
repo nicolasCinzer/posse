a04978dd49672a10020d7c6910263ea09b1ff8c6
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-11 22:33:29 -0500

    Nav init

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 type Props = {}
 
 export default function Navbar({}: Props) {
   return (
-    <nav>
+    <nav className='flex justify-evenly items-center text-acc-color py-2 border-b mx-4'>
       <Link href={'/prs'}>PRs</Link>
-      <Link href={'/'}></Link>
+      <Link
+        href={'/'}
+        className='text-2xl hover:text-comp-color transition-all duration-200 hover:tracking-widest'
+      >
+        POSSE
+      </Link>
+      <Link href={'/exercises'}>EXERCISES</Link>
     </nav>
   )
 }
