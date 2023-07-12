8007a5e4739189749e5392dc6220ebc4ceb23aed
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-12 00:18:36 -0500

    Programs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Programs from '../components/home/Programs'
+
 export default function Home() {
-  return <main className={``}></main>
+  return (
+    <main className='p-4'>
+      <Programs />
+    </main>
+  )
 }
