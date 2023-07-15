66b6949919f23c0a1fabd32507523eea3b593b71
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-15 08:08:58 -0500

    Add link to exercise title

--- src/app/exercises/components/ExercisesList.tsx
+++ src/app/exercises/components/ExercisesList.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 type Props = {
   exercises: Exercise[]
   movements: Movement[]
@@ -11,7 +13,13 @@ export default function ExercisesList({ exercises, movements }: Props) {
           key={exercise.id}
           className='border px-4 py-2 grid grid-cols-3 gap-2 items-center'
         >
-          <div>{exercise.name}</div>
+          <Link
+            href={`/exercise/${exercise.id}`}
+            className='group text-acc-color transition duration-300 w-max'
+          >
+            {exercise.name}
+            <span className='block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-acc-color'></span>
+          </Link>
           <div className=''>
             <div className='w-max px-3 py-1 rounded-full bg-comp-color'>
               {movements.find(movement => movement.id === exercise.movementId)?.name || ''}
