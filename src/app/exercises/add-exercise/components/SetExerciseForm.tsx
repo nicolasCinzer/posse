4ac2fbf753fcf61d52cebc08a9bcc50325bf3086
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-19 22:41:09 -0500

    Add exercise components

--- src/app/exercises/add-exercise/components/SetExerciseForm.tsx
+++ src/app/exercises/add-exercise/components/SetExerciseForm.tsx
@@ -1,54 +1,81 @@
 'use client'
 
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { ExerciseTypeEnum } from '@/lib/enums'
+import { ExercisesList } from '../../components'
 import Button from '@/src/components/ui/Button'
 import Select from '@/src/components/ui/Select'
+import { addExercise } from '@/lib'
 
 type Props = {
   movements: Movement[]
+  exercises: Exercise[]
 }
 
-export default function SetExerciseForm({ movements }: Props) {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+export default function SetExerciseForm({ movements, exercises }: Props) {
+  const [movementType, setMovementType] = useState<Partial<Exercise>>({ movementId: 0 })
+  const [exerciseType, setExerciseType] = useState<ExerciseType>('Basic')
+  const [exerciseName, setExerciseName] = useState<string>('')
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
+    await addExercise({
+      name: exerciseName,
+      movementId: movementType.movementId,
+      type: exerciseType
+    })
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='bg-acc-color text-dom-color p-4 rounded-md grid grid-cols-2 gap-4'
-    >
-      <h4 className='text-lg col-span-2'>Exercise Name</h4>
-      <input
-        type='text'
-        className='col-span-2 w-full border-b-2 border-dom-color bg-acc-color outline-none px-2 pb-2 focus:border-comp-color'
-        placeholder='Bench Press...'
-      />
-      <div className='flex justify-center gap-4 items-center'>
-        <label htmlFor='movement-select-add-exercises'>Movement</label>
-        <Select
-          id='movement-select-add-exercises'
-          options={['None', ...movements.map(movement => movement.name)]}
-          bgColor='acc'
-          style='border-b border-dom-color'
-        />
-      </div>
-      <div className='flex justify-center gap-4 items-center'>
-        <label htmlFor='type-select-add-exercises'>Type</label>
-        <Select
-          id='type-select-add-exercises'
-          options={Object.values(ExerciseTypeEnum).filter(value => isNaN(Number(value)))}
-          bgColor='acc'
-          style='border-b border-dom-color'
-        />
-      </div>
-      <Button
-        style='col-span-2'
-        colors={{ text: 'acc', textHover: 'acc', bg: 'comp', bgHover: 'dom' }}
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className='bg-acc-color text-dom-color p-4 rounded-md grid grid-cols-2 gap-4'
       >
-        Add
-      </Button>
-    </form>
+        <h4 className='text-lg col-span-2'>Exercise Name</h4>
+        <input
+          type='text'
+          className='col-span-2 w-full border-b-2 border-dom-color bg-acc-color outline-none px-2 pb-2 focus:border-comp-color'
+          placeholder='Bench Press...'
+          onChange={e => setExerciseName(e.target.value)}
+        />
+        <div className='flex justify-center gap-4 items-center'>
+          <label htmlFor='movement-select-add-exercises'>Movement</label>
+          <Select
+            id='movement-select-add-exercises'
+            options={[{ id: 0, name: 'None' }, ...movements]}
+            bgColor='acc'
+            style='border-b border-dom-color'
+            onChange={e => setMovementType({ movementId: parseInt(e.target.value) })}
+          />
+        </div>
+        <div className='flex justify-center gap-4 items-center'>
+          <label htmlFor='type-select-add-exercises'>Type</label>
+          <Select
+            id='type-select-add-exercises'
+            options={Object.values(ExerciseTypeEnum).filter(value => isNaN(Number(value)))}
+            bgColor='acc'
+            style='border-b border-dom-color'
+            onChange={e => setExerciseType(e.target.value)}
+          />
+        </div>
+        <Button
+          style='col-span-2'
+          colors={{ text: 'acc', textHover: 'acc', bg: 'comp', bgHover: 'dom' }}
+        >
+          Add
+        </Button>
+      </form>
+      <div className='border'></div>
+      <ExercisesList
+        exercises={
+          exerciseType && movementType.movementId
+            ? exercises.filter(exercise => exercise.type === exerciseType && exercise.movementId === movementType.movementId)
+            : exercises.filter(exercise => exercise.movementId === movementType.movementId || exercise.type === exerciseType)
+        }
+        movements={movements}
+      />
+    </>
   )
 }
--- src/app/exercises/add-exercise/page.tsx
+++ src/app/exercises/add-exercise/page.tsx
@@ -1,15 +1,17 @@
-import { getMovements } from '@/lib'
+import { getExercises, getMovements } from '@/lib'
 import SetExerciseForm from './components/SetExerciseForm'
+import { ExercisesList } from '../components'
 
 type Props = {}
 
 export default async function AddExercisePage({}: Props) {
   const movements: Movement[] = await getMovements()
+  const exercises: Exercise[] = await getExercises({})
 
   return (
-    <main className='p-4 '>
+    <main className='p-4 flex gap-4 flex-col'>
       <h2 className='text-xl'>Add Exercise</h2>
-      <SetExerciseForm movements={movements} />
+      <SetExerciseForm movements={movements} exercises={exercises}/>
     </main>
   )
 }
