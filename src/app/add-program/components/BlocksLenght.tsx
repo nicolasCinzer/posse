a809e85c857ec9166ed6edb62a39a3894d06c16a
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-26 01:05:11 -0500

    Delete unnecesary component

--- src/app/add-program/components/BlocksLenght.tsx
+++ src/app/add-program/components/BlocksLenght.tsx
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from 'react'
 import { Movements } from './'
 
 type Props = {
--- src/app/add-program/components/index.ts
+++ src/app/add-program/components/index.ts
@@ -1,8 +1,7 @@
-import AddProgramForm from './AddProgramForm'
 import BlockTypeLength from './BlockTypeLength'
 import BlocksLength from './BlocksLenght'
 import MovementItem from './MovementItem'
 import Movements from './Movements'
 import Program from './Program'
 
-export { AddProgramForm, BlocksLength, BlockTypeLength, MovementItem, Movements, Program }
+export { BlocksLength, BlockTypeLength, MovementItem, Movements, Program }
