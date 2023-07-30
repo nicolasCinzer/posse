52be86e7616ca27c4941de82a86446fcd03a1c7f
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-07-29 22:49:47 -0500

    Program Title

--- src/app/program/[name]/page.tsx
+++ src/app/program/[name]/page.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react'
 import { getPrograms } from '@/lib'
 import { Metadata } from 'next'
+import { UnderlineEffect } from '@/src/components/ui'
 
 type Props = { params: { name: string } }
 
@@ -22,7 +23,13 @@ export async function generateMetadata({ params: { name } }: Props): Promise<Met
 export default function Program({ params: { name } }: Props) {
   const programName = useMemo(() => decodeURI(name), [name])
 
-  return <div>{programName}</div>
+  return (
+    <main className='p-4'>
+      <h1 className='text-2xl group w-max'>
+        <UnderlineEffect>{programName}</UnderlineEffect>
+      </h1>
+    </main>
+  )
 }
 
 export async function generateStaticParams() {
