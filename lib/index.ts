647e4e615fed05e0b085c35f59eab378496038a2
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-08-04 15:42:20 -0500

    Update INDEX

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,8 @@ import getBlockTypes from './getBlockTypes'
 import getMovements from './getMovements'
 import getPrograms from './getPrograms'
 import getExercises from './getExercises'
+import getBlocks from './getBlocks'
 import addExercise from './addExercise'
 import addProgram from './addProgram'
 
-export { getBlockTypes, getExercises, getMovements, getPrograms, addExercise, addProgram }
+export { getBlockTypes, getExercises, getMovements, getPrograms, getBlocks, addExercise, addProgram }
