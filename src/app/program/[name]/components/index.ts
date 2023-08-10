fb3bcc992ee96929ac8442e1fb7155fb5982e2f7
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-08-09 23:44:22 -0500

    Index

---
+++ src/app/program/[name]/components/index.ts
@@ -0,0 +1,6 @@
+import Movements from './Movements'
+import MovementItem from './MovementItem'
+import BlockTypeLength from './BlockTypeLength'
+import DaysTable from './DaysTable'
+
+export { Movements, MovementItem, BlockTypeLength, DaysTable }
