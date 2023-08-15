4d662d29fe8155dff591a9352f8060bee59512cb
nicolasCinzer <nicolas.cinzer00@gmail.com>
2023-08-14 23:14:11 -0500

    Add block Context feature

--- src/context/ProgramContext.tsx
+++ src/context/ProgramContext.tsx
@@ -1,25 +1,25 @@
 'use client'
-import { createContext, useContext, Dispatch, useReducer, ReactElement, useMemo } from 'react'
+import { createContext, useContext,  useReducer, ReactElement, useMemo } from 'react'
 
 const REDUCER_ACTION_TYPE = {
   PROGRAM: 'PROGRAM',
-  BLOCKS: { ADD: 'ADD', REMOVE: 'REMOVE', UPDATE: 'UPDATE' },
-  RUTINE_DAY: { ADD: 'ADD', REMOVE: 'REMOVE', UPDATE: 'UPDATE' },
-  RUTINE_DAY_EXERCISE: { ADD: 'ADD', REMOVE: 'REMOVE', UPDATE: 'UPDATE' }
+  BLOCKS: { ADD: 'BLOCK_ADD', REMOVE: 'BLOCK_REMOVE', UPDATE: 'BLOCK_UPDATE' },
+  RUTINE_DAY: { ADD: 'RUTINE_DAY_ADD', REMOVE: 'RUTINE_DAY_REMOVE', UPDATE: 'RUTINE_DAY_UPDATE' },
+  RUTINE_DAY_EXERCISE: { ADD: 'RUTINE_DAY_EXERCISE_ADD', REMOVE: 'RUTINE_DAY_EXERCISE_REMOVE', UPDATE: 'RUTINE_DAY_EXERCISE_UPDATE' }
 }
 
 export type ReducerActionType = typeof REDUCER_ACTION_TYPE
 
 export type ReducerAction = {
   type: string
-  payload?: Program | Block[] | RutineDay[] | RutineDayExercise[]
+  payload?: Partial<Program> | Partial<Block>[] | Partial<RutineDay>[] | Partial<RutineDayExercise>[]
 }
 
 type ProgramType = {
   program: Program
-  blocks: Block[]
-  rutineDay: RutineDay[]
-  rutineDayExercise: RutineDayExercise[]
+  blocks: Partial<Block>[]
+  rutineDay: Partial<RutineDay>[]
+  rutineDayExercise: Partial<RutineDayExercise>[]
 }
 
 const initProgramState: ProgramType = {
@@ -45,6 +45,36 @@ const reducer = (state: ProgramType, action: ReducerAction): ProgramType => {
 
       return { ...state, program: { id, name, duration, workDaysNumber } }
     }
+    case REDUCER_ACTION_TYPE.BLOCKS.ADD: {
+      if (!action.payload) {
+        throw new Error('Block data is missing on BLOCK ADD Action.')
+      }
+
+      const blocks = action.payload as Block[]
+
+      return { ...state, blocks: blocks }
+    }
+    case REDUCER_ACTION_TYPE.BLOCKS.UPDATE: {
+      if (!action.payload) {
+        throw new Error('Block data is missing on BLOCK UPDATE Action.')
+      }
+
+      const [updatedBlock] = action.payload as Partial<Block>[]
+
+      let blocks: Partial<Block>[]
+      if (updatedBlock.id) {
+        blocks = state.blocks.map(block => {
+          if (block.movementId === updatedBlock.movementId && block.typeId === updatedBlock.typeId) {
+            return { ...block, duration: updatedBlock.duration }
+          }
+          return block
+        })
+      } else {
+        blocks = [...state.blocks, updatedBlock]
+      }
+
+      return { ...state, blocks: blocks }
+    }
     default:
       throw new Error('Undefined ACTION Type.')
   }
@@ -55,7 +85,11 @@ const useProgramContext = (initProgramState: ProgramType) => {
 
   const REDUCER_ACTIONS = useMemo(() => REDUCER_ACTION_TYPE, [])
 
-  return { dispatch, programState, REDUCER_ACTIONS }
+  const currentsMovements = [...new Set(programState.blocks.map(block => block.movementId))]
+
+  console.log(currentsMovements)
+
+  return { dispatch, programState, REDUCER_ACTIONS, initProgramState: programState }
 }
 
 type useProgramContextType = ReturnType<typeof useProgramContext>
@@ -63,7 +97,8 @@ type useProgramContextType = ReturnType<typeof useProgramContext>
 const initProgramContextState: useProgramContextType = {
   programState: initProgramState,
   dispatch: () => {},
-  REDUCER_ACTIONS: REDUCER_ACTION_TYPE
+  REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+  initProgramState: initProgramState
 }
 
 const ProgramContext = createContext<useProgramContextType>(initProgramContextState)
